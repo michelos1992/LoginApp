0ad66ca6a15ce96a566b2de04f6818657a4d9396
Misiu <michel.wojciech@gmail.com>
2019-02-23 16:25:14 +0100

    Create components and services

--- myLoginApp/src/app/app.module.ts
+++ myLoginApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
